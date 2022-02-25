81610e35788ad9380ada4db1782422b0671020c0
Dejan Prole <shimids89@gmail.com>
2022-02-25 03:35:17 +0100

    Add ide helper

---
+++ _ide_helper_models.php
@@ -0,0 +1,115 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\Brand
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $title
+ * @property string $slug
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereSlug($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Brand whereUuid($value)
+ */
+	class Brand extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\JwtToken
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $unique_id
+ * @property string $token_title
+ * @property array|null $restrictions
+ * @property array|null $permissions
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null $expires_at
+ * @property int|null $last_used_at
+ * @property int|null $refreshed_at
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken query()
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereExpiresAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereLastUsedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken wherePermissions($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereRefreshedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereRestrictions($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereTokenTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereUniqueId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereUserId($value)
+ */
+	class JwtToken extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $first_name
+ * @property string $last_name
+ * @property bool $is_admin
+ * @property string $email
+ * @property int|null $email_verified_at
+ * @property string $password
+ * @property string|null $avatar
+ * @property string $address
+ * @property string $phone_number
+ * @property bool $is_marketing
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property int|null $last_login_at
+ * @property-read string $name
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\JwtToken[] $jwtTokens
+ * @property-read int|null $jwt_tokens_count
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsAdmin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsMarketing($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUuid($value)
+ */
+	class User extends \Eloquent implements \Tymon\JWTAuth\Contracts\JWTSubject {}
+}
+
